b2fc71d725ecf439c01a0d4b7aefd2e9d1f238f2
ammaraljewary <ammaraljewary@gmail.com>
2020-10-04 19:41:21 +0300

    api changed

--- app/src/main/java/com/example/covid_19tracker/CustomAdapter.java
+++ app/src/main/java/com/example/covid_19tracker/CustomAdapter.java
@@ -1,17 +1,71 @@
 package com.example.covid_19tracker;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class CustomAdapter extends BaseAdapter {
 
     Context c;
-    String[] names = {"World","Afghanistan", "Akrotiri","Albania","Algeria","American Samoa","Andorra","Angola","Anguilla",
-            "Antarctica", "Finland","Iraq","Syria"};
+    String[] names = new String[300];
+
+    private JSONArray allCountries;
+    private ArrayList<String> country = new ArrayList<>();
+
+
+    String urlCountries = "https://covid19.mathdro.id/api/countries";
+    StringRequest requestCountries = new StringRequest(Request.Method.GET, urlCountries, new Response.Listener<String>() {
+        @Override
+
+        public void onResponse(String response) {
+            try {
+
+                JSONObject jsonObject = new JSONObject(response.toString());
+                allCountries = jsonObject.getJSONArray("countries");
+                names[0] = "World";
+                for(int i=1; i < allCountries.length(); i++){
+                    names[i]=allCountries.getJSONObject(i).getString("name");
+                    Log.d("json", names[i]);
+                }
+
+                Log.d("before all countries", "yes");
+
+                Log.d( "onResponse", String.valueOf(allCountries));
+            } catch (JSONException e) {
+                Log.d("error", "there is ");
+                e.printStackTrace();
+            }
+        }
+    },
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error)
+                {
+                    Toast.makeText(c,
+                            error.getMessage(),
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+
+
 
 
     public CustomAdapter(Context c) {
--- app/src/main/java/com/example/covid_19tracker/MainActivity.java
+++ app/src/main/java/com/example/covid_19tracker/MainActivity.java
@@ -7,8 +7,10 @@ import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
@@ -24,6 +26,7 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -33,7 +36,9 @@ public class MainActivity extends AppCompatActivity {
 
     //UI Views
     TextView all, kuolematValue, atiivisetValue,toipuneetValue;
-
+    private int selectedCountryIndex;
+    private String selectedCountryName = "finland";
+    CustomAdapter adapter = new CustomAdapter(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,17 +50,68 @@ public class MainActivity extends AppCompatActivity {
         atiivisetValue = findViewById(R.id.AtiivisetValue);
         toipuneetValue = findViewById(R.id.ToipuneetValue);
         Spinner list = findViewById(R.id.spinner);
-        CustomAdapter adapter = new CustomAdapter(this);
+
         list.setAdapter(adapter);
+        list.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("on item", String.valueOf(position));
+                changedCountry = true;
+                selectedCountryIndex = position;
+                fetchdata();
+                return;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                return;
+            }
+        });
+
         fetchdata();
 
     }
     private String allCases,recoverdCases, deathsCases, aktiiviset;
+    private JSONArray allCountries;
+    private String country;
+    private boolean changedCountry = false;
+    private String url = "https://covid19.mathdro.id/api";
     //get request to the api data
     private void fetchdata() {
-        RequestQueue queue = Volley.newRequestQueue(this);
+        final RequestQueue queue = Volley.newRequestQueue(this);
+        String urlCountries = "https://covid19.mathdro.id/api/countries";
 
-        String url = "https://covid19.mathdro.id/api    ";
+        final StringRequest requestSelectCountry = new StringRequest(Request.Method.GET, urlCountries, new Response.Listener<String>() {
+            @Override
+
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response.toString());
+                    allCountries = jsonObject.getJSONArray("countries");
+                        if(selectedCountryIndex !=0){
+                            country=allCountries.getJSONObject(selectedCountryIndex).getString("name");
+                            selectedCountryName = country;
+                            url = url + "/countries/" + selectedCountryName;
+                            Log.d("select country name ", country);
+                        }
+
+
+                } catch (JSONException e) {
+                    Log.d("error", "there is ");
+                    e.printStackTrace();
+                }
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error)
+                    {
+                        Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
         StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
 
@@ -69,13 +125,12 @@ public class MainActivity extends AppCompatActivity {
                     aktiiviset = String.valueOf(
                             Integer.valueOf(allCases) -(
                                     Integer.valueOf(recoverdCases) + Integer.valueOf(deathsCases)
-                                    )
+                            )
                     );
                     all.setText(allCases);
                     toipuneetValue.setText(recoverdCases);
                     atiivisetValue.setText(aktiiviset);
                     kuolematValue.setText(deathsCases);
-
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -88,10 +143,17 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 });
-
+        queue.add(adapter.requestCountries);
+        queue.add(requestSelectCountry);
         queue.add(request);
+        if(changedCountry == true){
+            queue.add(request);
+            changedCountry = false;
+        }
+
 
     }
+
 
     public void OhjeetActivity(View view) {
         Intent intent = new Intent(this, OhjeetActivity.class);
